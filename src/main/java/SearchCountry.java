35ac710e5d22062f8c7f84648e9173552cd04168
Andrew Plastinin <SBT-Plastinin-AS@sberbank.ru>
2017-07-03 15:50:47 +0300

    new file

---
+++ src/main/java/SearchCountry.java
@@ -0,0 +1,42 @@
+package main.java.country;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import main.java.country.Countries;
+import main.java.country.CountriesJson;
+import main.java.country.Country;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+@Path("/")
+public class SearchCountry {
+
+    public Response jsonSendListCountries(List<Country> countries){
+        JSONObject jsonCountryList = new JSONObject();
+        for (Country oneCountryInResult: countries) {
+            jsonCountryList.put(Integer.toString(oneCountryInResult.getKeyValue()), oneCountryInResult.getNameCountry());
+        }
+        return Response.status(Response.Status.OK).entity(jsonCountryList.toString()).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public Response searchCountryWithoutName() throws JSONException {
+        Countries countries = new CountriesJson();
+        return jsonSendListCountries(countries.getAllCountry());
+    }
+
+    @Path("{partName}")
+    @GET
+    @Produces("application/json")
+    public Response searchCountryWithPartName(@PathParam("partName") String partName) throws JSONException {
+        Countries countries = new CountriesJson();
+        return jsonSendListCountries(countries.getCountryBySearch(partName));
+    }
+}
