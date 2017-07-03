c5db909d4953835f3f968666c058b4eed41e3ecd
Andrew Plastinin <SBT-Plastinin-AS@sberbank.ru>
2017-07-03 16:33:36 +0300

    new file

---
+++ src/test/java/CountriesTest.java
@@ -0,0 +1,10 @@
+import main.java.country.Countries;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+public class CountriesTest {
+    @Test
+    public void testCalA() throws Exception {
+        Countries countries = new Countries();
+    }
+}
