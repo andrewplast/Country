57b8cf8447e595baad1e5217ad6c00c235ff0784
Andrew Plastinin <SBT-Plastinin-AS@sberbank.ru>
2017-07-04 17:26:31 +0300

    edit

---
+++ src/test/java/country/dao/XLSCountryDAOTest.java
@@ -0,0 +1,26 @@
+package country.dao;
+
+import static org.junit.Assert.*;
+
+public class XLSCountryDAOTest {
+    @org.junit.Test
+    public void getAll() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void findCountries() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void delete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+
+    }
+
+}
