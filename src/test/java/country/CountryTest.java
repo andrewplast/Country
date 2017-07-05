6fadac44a89d07b4facb01baace4fbdc3b27b9e0
Andrew Plastinin <SBT-Plastinin-AS@sberbank.ru>
2017-07-05 12:16:12 +0300

    edit

--- src/test/java/country/CountryTest.java
+++ src/test/java/country/CountryTest.java
@@ -3,11 +3,11 @@ package country;
 import country.dao.Country;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class CountryTest {
     @Test
     public void getName() throws Exception {
-        assertTrue("Имя созданной страны не совпадает с именем в конструкторе", "Test"==new Country("Test").getName());
+        assertTrue("Имя созданной страны не совпадает с именем в конструкторе", "Test".equals(new Country("Test").getName()));
     }
 }
